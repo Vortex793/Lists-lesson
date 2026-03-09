c399ffbbbf58e45069d6a467835abd979eebebac
339821175 <339821175@2260I-L29-015>
2026-03-09 13:15:19 -0400

    Add project files.

---
+++ Lists/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Trinity", "Neo", "Morpheus", "Smith" };
+            Console.WriteLine(names[1] + "is 'The One'.");
+        }
+    }
+}
+
