770ae95f1e7a6fb82476924b52b4826817265f00
Vortex793 <Curtisapf94@gmail.com>
2026-03-09 22:19:53 -0400

    random added

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,24 @@ namespace Lists
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string> { "Trinity", "Neo", "Morpheus", "Smith" };
+
+            Random generator = new Random();
+
+            List<string> names = new List<string> { "Trinity", "Neo", "Morpheus", "Cypher" };
             Console.WriteLine(names[1] + "is 'The One'.");
+            Console.WriteLine(names[3] + "is the bad guy");
+            Console.WriteLine("There are " + names.Count + " names in my list.");
+            names[3] = "Agent Smith";
+            Console.WriteLine(names[3] + "is the bad guy.");
+            Console.ReadLine();
+            
+            int randomIndex = generator.Next(0, names.Count);
+            Console.WriteLine("Random name: " + names[randomIndex]);
+
+
+            //number in the square brackets is reffering to which part of the list 
+            //2. Console.Writeline
+            //
         }
     }
 }
